0d46e75ab217f1a4dd71727249a7cd2d83893209
oxy18 <84609520+0x18e@users.noreply.github.com>
2025-07-08 19:00:44 -0400

    refactor: remove unnecessary headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,15 @@
 #include "Engine.h"
 
-#include "Mesh.h"
-#include "assimp/Importer.hpp"
-#include "assimp/scene.h"
 // important early goals
 // use RAII
 // kernighan and ritchie convention
 // hungarian notation
 // use public, protected, private conventions.
 
+int main() {
 
-int main(int argc, char* argv[]){
 	Engine engine;
 	engine.Run();
+
 	return 0;
 }
